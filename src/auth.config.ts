db9e28cd4bae40fe2b0e2ef58f824ba4f26636ea
arch linux <hoangsonj4f@gmail.com>
2024-09-08 15:42:50 +0700

    environment variables

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -60,8 +60,14 @@ export default {
         return user
       },
     }),
-    GitHub,
-    Google,
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
   pages: {
     signIn: '/sign-in',
