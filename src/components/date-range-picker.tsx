65e6d1f3da9bd350582178d7dc8130d730625203
sonwjnn <hoangsonj4f@gmail.com>
2024-11-26 05:37:10 +0700

    fix date range error type

--- src/components/date-range-picker.tsx
+++ src/components/date-range-picker.tsx
@@ -3,14 +3,13 @@ import { format } from 'date-fns'
 import { CalendarIcon } from 'lucide-react'
 import { DateRange, SelectRangeEventHandler } from 'react-day-picker'
 
-
 import { Button } from './ui/button'
 import { Calendar } from './ui/calendar'
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
 
 interface DateRangePickerProps {
-  date: DateRange
-  setDate: SelectRangeEventHandler
+  date: DateRange | undefined
+  setDate: (date: DateRange | undefined) => void
 }
 
 export function DateRangePicker({ date, setDate }: DateRangePickerProps) {
@@ -22,10 +21,11 @@ export function DateRangePicker({ date, setDate }: DateRangePickerProps) {
           {date?.from ? (
             date.to ? (
               <>
-                {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
+                {format(date.from, 'LLL dd, y')} -{' '}
+                {format(date.to, 'LLL dd, y')}
               </>
             ) : (
-              format(date.from, "LLL dd, y")
+              format(date.from, 'LLL dd, y')
             )
           ) : (
             <span>Pick a date</span>
@@ -43,4 +43,3 @@ export function DateRangePicker({ date, setDate }: DateRangePickerProps) {
     </Popover>
   )
 }
-
