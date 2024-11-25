ef9380eae7d8ffc13f663c77a4f4fd7d95a12195
sonwjnn <hoangsonj4f@gmail.com>
2024-11-26 06:02:45 +0700

    unused dbml

--- src/lib/dbml.ts
+++
@@ -1,8 +0,0 @@
-import * as schema from '@/db/schema'
-import { pgGenerate } from 'drizzle-dbml-generator'
-
-const out = './schema.dbml'
-const relational = true
-pgGenerate({ schema, out, relational })
-console.log('✅ Created the schema.dbml file')
-console.log('⏳ Creating the erd.svg file')
